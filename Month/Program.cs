1a9a8f2830aafdbe626ffad5db201794e3a79bbe
Marie Cowen <marie..cowen.87@gmail.com>
2019-02-14 16:29:27 -0600

    CoderGirl Homework Solutions

--- Month/Program.cs
+++ Month/Program.cs
@@ -9,13 +9,63 @@ namespace Month
             Console.Write("Enter the number of the month: ");
 
             // Save the input to a variable.
-
-            // Use the if statment to update the monthName variable based on the user's input.
+            string monthNumber = Console.ReadLine();
             string monthName = string.Empty;
-
-
+            // Use the if statment to update the monthName variable based on the user's input.
+            if (monthNumber == "1")
+            {
+                monthName = "January";
+            }
+            else if (monthNumber == "2")
+            {
+                monthName = "February";
+            }
+            else if (monthNumber == "3")
+            {
+                monthName = "March";
+            }
+            else if (monthNumber == "4")
+            {
+                monthName = "April";
+            }
+            else if (monthNumber == "5")
+            {
+                monthName = "May";
+            }
+            else if (monthNumber == "6")
+            {
+                monthName = "June";
+            }
+            else if (monthNumber == "7")
+            {
+                monthName = "July";
+            }
+            else if (monthNumber == "8")
+            {
+                monthName = "August";
+            }
+            else if (monthNumber == "9")
+            {
+                monthName = "September";
+            }
+            else if (monthNumber == "10")
+            {
+                monthName = "October";
+            }
+            else if (monthNumber == "11")
+            {
+                monthName = "November";
+            }
+            else if (monthNumber == "12")
+            {
+                monthName = "December";
+            }
+            else
+            {
+                monthName = "not a valid month";
+            }
+         
             Console.WriteLine($"The name of the month you entered is {monthName}");
-
             Console.ReadLine();
         }
     }
